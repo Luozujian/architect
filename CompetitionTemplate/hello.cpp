509d1ea5d1fe94391f96481022b20222f13c2890
luozujian1 <358492589@qq.com>
2022-06-06 21:19:52 +0800

    提交初始化代码

---
+++ CompetitionTemplate/hello.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Administrator on 2022-06-06.
+//
+#include <cstdio>
+using namespace std;
+int main() {
+    
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm.dir/CompetitionTemplate/hello.cpp.obj"
+  "algorithm.exe"
+  "algorithm.exe.manifest"
+  "algorithm.pdb"
+  "libalgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
